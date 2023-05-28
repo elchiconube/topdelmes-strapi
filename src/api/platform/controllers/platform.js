323d589d00555c8b64d4327aef2d959e80c66c87
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-05-28 12:43:29 +0200

    feat: populate platform

--- src/api/platform/controllers/platform.js
+++ src/api/platform/controllers/platform.js
@@ -1,9 +1,45 @@
-'use strict';
+"use strict";
 
 /**
  * platform controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::platform.platform');
+const uid = "api::platform.platform";
+
+const components = {
+  contents: true,
+};
+
+module.exports = createCoreController(uid, () => {
+  return {
+    async find(ctx) {
+      if (ctx.query.populate === "*") {
+        const entity = await strapi.entityService.findMany(uid, {
+          ...ctx.query,
+          populate: components,
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+      }
+      return super.find(ctx);
+    },
+    async findOne(ctx) {
+      const { id } = ctx.request.params;
+
+      if (ctx.query.populate === "*") {
+        const entity = await strapi.entityService.findOne(uid, id, {
+          ...ctx.query,
+          populate: components,
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+      }
+
+      return super.findOne(ctx);
+    },
+  };
+});
